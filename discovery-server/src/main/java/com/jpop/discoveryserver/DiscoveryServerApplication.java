6d964b164cf06df0a73491d4e7a29f2970e4f7b0
Bishal Chhotray <Bishal_Chhotray@epam.com>
2019-11-08 17:39:23 +0530

    discovery service updated code

--- discovery-server/src/main/java/com/jpop/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/jpop/discoveryserver/DiscoveryServerApplication.java
@@ -1,15 +1,15 @@
-package com.jpop.EurekaNamingServer;
+package com.jpop.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaNamingServerApplication {
+@SpringBootApplication
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaNamingServerApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
--- discovery-server/src/test/java/com/jpop/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/jpop/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,13 +1,14 @@
-package com.jpop.EurekaNamingServer;
+package com.jpop.discoveryserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaNamingServerApplicationTests {
+public class DiscoveryServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
